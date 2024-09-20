9dd163d44bdf220c774a812cb84515331f46f0e2
Hüseyin Tuğrul BÜYÜKIŞIK <huseyin.tugrul.buyukisik@gmail.com>
2024-09-20 12:30:50 +0300

    Update quicksort_test.cu
    yet another bug fix

--- quicksort_test.cu
+++ quicksort_test.cu
@@ -24,9 +24,9 @@ __global__ void resetNumTasks(unsigned int* arr, unsigned int* leftMem, unsigned
     {
         numTasks[0] = 0;
         //printf("\n %i \n", n);
-        quickSortWithoutStreamCompaction <<<n, 64,0, cudaStreamTailLaunch >>> (arr, leftMem, rightMem, depth, numTasks,tasks,tasks2);
+        quickSortWithoutStreamCompaction << <n, 64, 0, cudaStreamTailLaunch >> > (arr, leftMem, rightMem, depth, numTasks, tasks, tasks2);
     }
-    
+
 }
 
 __global__ void copyTasksBack(unsigned int* arr, unsigned int* leftMem, unsigned int* rightMem, int depth, unsigned int* numTasks,
@@ -35,19 +35,19 @@ __global__ void copyTasksBack(unsigned int* arr, unsigned int* leftMem, unsigned
     const int id = threadIdx.x;
     const int n = numTasks[0];
     const int steps = 1 + n / 1024;
- 
+
     for (int i = 0; i < steps; i++)
     {
         const int curId = id + i * 1024;
-        if(curId < n)        
+        if (curId < n)
         {
-            tasks[curId*2] = tasks2[curId*2];
-            tasks[curId * 2+1] = tasks2[curId * 2+1];
+            tasks[curId * 2] = tasks2[curId * 2];
+            tasks[curId * 2 + 1] = tasks2[curId * 2 + 1];
         }
     }
     if (id == 0)
     {
-        resetNumTasks <<<1, 1,0, cudaStreamTailLaunch>>> (arr, leftMem, rightMem,depth,numTasks,tasks, tasks2);
+        resetNumTasks << <1, 1, 0, cudaStreamTailLaunch >> > (arr, leftMem, rightMem, depth, numTasks, tasks, tasks2);
     }
 }
 
@@ -56,19 +56,19 @@ __global__ void copyTasksBack(unsigned int* arr, unsigned int* leftMem, unsigned
 //              start stop  start stop  start stop  start stop  ---> tasks buffer
 //              block 0     block 1     block 2     block 3     ---> cuda blocks
 __global__ void quickSortWithoutStreamCompaction(
-    unsigned int* arr, unsigned int* leftMem, unsigned int* rightMem, int depth, unsigned int * numTasks,
-    int* tasks,int*tasks2)
+    unsigned int* arr, unsigned int* leftMem, unsigned int* rightMem, int depth, unsigned int* numTasks,
+    int* tasks, int* tasks2)
 {
     const int gr = gridDim.x;
-    
+
     // 1 block = 1 chunk of data
     const int gid = blockIdx.x;
     const int id = threadIdx.x;
-    
+
 
 
     const int startIncluded = tasks[gid * 2];
-    const int stopIncluded = tasks[gid * 2+1];
+    const int stopIncluded = tasks[gid * 2 + 1];
     const int num = stopIncluded - startIncluded + 1;
 
 
@@ -84,7 +84,7 @@ __global__ void quickSortWithoutStreamCompaction(
     unsigned int pivot = arr[stopIncluded];
 
 
-  
+
     __shared__ int indexLeft;
     __shared__ int indexRight;
     int indexLeftR = 0;
@@ -105,12 +105,12 @@ __global__ void quickSortWithoutStreamCompaction(
             {
                 const auto data = arr[curId + startIncluded];
                 if (data < pivot)
-                    leftMem[startIncluded + atomicAdd(&indexLeft[0], 1)] = data;
+                    leftMem[startIncluded + atomicAdd(&indexLeft, 1)] = data;
                 else
                 {
                     if (curId + startIncluded != stopIncluded)
                     {
-                        rightMem[startIncluded + atomicAdd(&indexRight[0], 1)] = data;
+                        rightMem[startIncluded + atomicAdd(&indexRight, 1)] = data;
                     }
                 }
             }
@@ -143,7 +143,7 @@ __global__ void quickSortWithoutStreamCompaction(
             const int curId = i * bd + id;
             if (curId + indexLeftR + startIncluded + 1 <= stopIncluded)
             {
-                arr[curId + indexLeftR + startIncluded+1] = rightMem[startIncluded + curId];
+                arr[curId + indexLeftR + startIncluded + 1] = rightMem[startIncluded + curId];
             }
         }
     }
@@ -163,7 +163,7 @@ __global__ void quickSortWithoutStreamCompaction(
                 const int index = atomicAdd(&numTasks[0], 1);
                 tasks2[index * 2] = startIncluded;
                 tasks2[index * 2 + 1] = startIncluded + nLeft - 1;
-                
+
             }
         }
 
@@ -175,37 +175,37 @@ __global__ void quickSortWithoutStreamCompaction(
                 const int index = atomicAdd(&numTasks[0], 1);
                 tasks2[index * 2] = startIncluded + nLeft + 1;
                 tasks2[index * 2 + 1] = stopIncluded;
-               
+
             }
         }
     }
 
-    if(id==0 && gid==0)        
-        copyTasksBack <<<1, 1024, 0, cudaStreamTailLaunch >>> (arr, leftMem, rightMem, depth, numTasks, tasks, tasks2);
+    if (id == 0 && gid == 0)
+        copyTasksBack << <1, 1024, 0, cudaStreamTailLaunch >> > (arr, leftMem, rightMem, depth, numTasks, tasks, tasks2);
 }
 
 
 
 __global__ void qSortMain(
     unsigned int* arr, unsigned int* leftMem, unsigned int* rightMem, int depth, unsigned int* numTasks,
-    int* tasks,int*tasks2)
+    int* tasks, int* tasks2)
 {
 
-    quickSortWithoutStreamCompaction<<<1, 64>>>(arr, leftMem, rightMem, depth, numTasks, tasks, tasks2);
+    quickSortWithoutStreamCompaction << <1, 64 >> > (arr, leftMem, rightMem, depth, numTasks, tasks, tasks2);
 }
 
 void test()
 {
     constexpr int n = 1024 * 1024;
-    unsigned int *data, *left, * right,*numTasks;
-    int* tasks,*tasks2;
+    unsigned int* data, * left, * right, * numTasks;
+    int* tasks, * tasks2;
     std::vector<unsigned int> hostData(n);
     std::vector<int> hostTasks(2);
     cudaMalloc(&data, n * sizeof(unsigned int));
     cudaMalloc(&left, n * sizeof(unsigned int));
     cudaMalloc(&right, n * sizeof(unsigned int));
-    cudaMalloc(&numTasks, 2*sizeof(unsigned int));
-    cudaMalloc(&tasks, n*sizeof(int ));
+    cudaMalloc(&numTasks, 2 * sizeof(unsigned int));
+    cudaMalloc(&tasks, n * sizeof(int));
     cudaMalloc(&tasks2, n * sizeof(int));
     unsigned int numTasksHost[2];
     for (int j = 0; j < 5; j++)
@@ -217,17 +217,17 @@ void test()
         numTasksHost[0] = 1; // launch 1 block first
         numTasksHost[1] = 0;
         hostTasks[0] = 0;
-        hostTasks[1] = n-1; // first block's chunk limits: 0 - n-1
+        hostTasks[1] = n - 1; // first block's chunk limits: 0 - n-1
         cudaMemcpy((void*)data, hostData.data(), n * sizeof(unsigned int), cudaMemcpyHostToDevice);
-        cudaMemcpy((void*)numTasks, &numTasksHost, 2*sizeof(unsigned int), cudaMemcpyHostToDevice);
+        cudaMemcpy((void*)numTasks, numTasksHost, 2 * sizeof(unsigned int), cudaMemcpyHostToDevice);
         cudaMemcpy((void*)tasks, hostTasks.data(), 2 * sizeof(int), cudaMemcpyHostToDevice); // host only gives 1 task with 2 parameters
-        qSortMain <<<1, 1 >>> (data,left,  right,0, numTasks, tasks, tasks2);
+        qSortMain << <1, 1 >> > (data, left, right, 0, numTasks, tasks, tasks2);
         cudaDeviceSynchronize();
         cudaMemcpy(hostData.data(), (void*)data, n * sizeof(unsigned int), cudaMemcpyDeviceToHost);
-        cudaMemcpy(&numTasksHost, (void*)numTasks,  2*sizeof(unsigned int), cudaMemcpyDeviceToHost);
+        cudaMemcpy(numTasksHost, (void*)numTasks, 2 * sizeof(unsigned int), cudaMemcpyDeviceToHost);
 
     }
-   
+
     bool err = false;
     for (int i = 0; i < n - 1; i++)
         if (hostData[i] > hostData[i + 1])
